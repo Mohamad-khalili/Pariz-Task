467acab877133341be593213e0d016e08604945f
Mohamad khalili <mohamad.khalili0605@gmail.com>
2026-08-28 20:38:56 +0400

    add paging load state

--- app/src/main/java/com/pariz/task/presentation/MoviesScreen.kt
+++ app/src/main/java/com/pariz/task/presentation/MoviesScreen.kt
@@ -1,6 +1,5 @@
 package com.pariz.task.presentation
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +13,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.TextAutoSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -25,9 +25,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
 import coil3.compose.AsyncImage
@@ -59,7 +61,23 @@ fun MovieItem(
         contentAlignment = Alignment.Center
     ) {
 
+        when {
+            movies.loadState.refresh is LoadState.Loading -> {
 
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+
+            movies.loadState.refresh is LoadState.Error -> {
+                val error = movies.loadState.refresh as LoadState.Error
+                Text(
+                    text = error.error.message ?: stringResource(R.string.something_went_wrong),
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+
+            movies.loadState.refresh is LoadState.NotLoading -> {
                 LazyColumn(modifier = modifier) {
                     items(
                         movies.itemCount,
@@ -77,9 +95,8 @@ fun MovieItem(
                     }
                 }
             }
-
-
-
+        }
+    }
 
 }
 
@@ -114,7 +131,7 @@ fun ItemRow(item: Movies) {
 
             Spacer(modifier = Modifier.width(24.dp))
 
-            Column() {
+            Column {
                 item.title?.let {
                     Text(
                         text = it,
